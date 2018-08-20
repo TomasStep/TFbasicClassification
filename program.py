1670ade0077678288695aa6094c1cc402eb39c6c
Unknown <ttomass@gmail.com>
2018-08-21 00:23:53 +0300

    updated program

--- program.py
+++ program.py
@@ -3,3 +3,21 @@ import tensorflow as tf
 import numpy as np
 
 print(tf.__version__)
+
+from tensorflow.contrib.learn.python.learn.datasets import base
+
+#Data files
+IRIS_TRAINING = "data_train.csv"
+IRIS_TEST = "data_test.csv"
+
+#Load datasets
+training_set = base.load_csv_with_header(filename=IRIS_TRAINING,
+                                         features_dtype=np.float32,
+                                         target_dtype=np.int)
+test_set = base.load_csv_with_header(filename=IRIS_TEST,
+                                     features_dtype=np.float32,
+                                     target_dtype=np.int)
+
+print(training_set.data)
+
+print(training_set.target)
